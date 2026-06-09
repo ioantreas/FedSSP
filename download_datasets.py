a4cf34283bee89ea6bc07f261b0281df4754b60c
Jacek Kulik <macienias@gmail.com>
2026-06-09 15:01:23 +0200

    Fix synthie dataset

--- download_datasets.py
+++ download_datasets.py
@@ -8,17 +8,20 @@ datasets = [
     "Peking_1", "OHSU", "PROTEINS",
     "IMDB-MULTI", "IMDB-BINARY",
     "Letter-high", "Letter-med", "Letter-low",
-    "SYNTHETICnew", "SYNTHIE", "SYNTHETIC", 
+    "SYNTHETICnew", "Synthie", "SYNTHETIC", 
     "DD", "REDDIT-BINARY", "FIRSTMM_DB"
 ]
 
 for name in datasets:
-    if name == "IMDB-BINARY":
-        ds = TUDataset(root, name, pre_transform=OneHotDegree(135, cat=False))
-    elif name == "IMDB-MULTI":
-        ds = TUDataset(root, name, pre_transform=OneHotDegree(88, cat=False))
-    elif "Letter" in name:
-        ds = TUDataset(root, name, use_node_attr=True)
-    else:
-        ds = TUDataset(root, name)
-    print(name, len(ds))
+    try:
+        if name == "IMDB-BINARY":
+            ds = TUDataset(root, name, pre_transform=OneHotDegree(135, cat=False))
+        elif name == "IMDB-MULTI":
+            ds = TUDataset(root, name, pre_transform=OneHotDegree(88, cat=False))
+        elif "Letter" in name:
+            ds = TUDataset(root, name, use_node_attr=True)
+        else:
+            ds = TUDataset(root, name)
+        print(name, len(ds))
+    except Exception as exc:
+        print(f"Skipping {name}: {exc}")
